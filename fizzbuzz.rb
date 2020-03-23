526a45e67314d75a927cae0218cc745470ad647b
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-23 18:16:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
- if int % 3 == 0
-   puts "Fizz"
-elsif int % 5 == 0
-   puts "Buzz"
-elsif [int % 3 == 0] && [int % 5 == 0]
+ if [int % 3 == 0] && [int % 5 == 0]
    puts "FizzBuzz"
+elsif int % 3 == 0
+   puts "Fizz"
+elsif  int % 5 == 0
+   puts "Buzz"
+   puts "Buzz"
 else int !% 3 || 5 == 0
    puts "nil"
  end
